f9f1445fa6ead85663d14ea1a7f5f907027ab96a
antony1140 <67793865+antony1140@users.noreply.github.com>
2026-03-24 22:15:00 -0400

    added gitignore, service framework

---
+++ utilities/agent-service.cs
@@ -0,0 +1,10 @@
+namespace service.agent;
+
+//TODO: Write all agent related services here, ie analyze(story Story)
+public class Agent {
+
+	public static async analyze_story() {
+
+	}
+
+}
---
+++ utilities/devops-service.cs
@@ -0,0 +1,37 @@
+namespace service.devops;
+
+//TODO: write all devops related methods here ie. fetch stories, insert tests, etc.
+public class ADO {
+
+	public static get_stories_by_project() {
+
+	}
+
+	public static get_stories_by_sprint() {
+
+	}
+
+	public static get_stories_by_feature() {
+
+	}
+
+	//will almost definitley use
+	public static get_story_by_id() {
+
+	}
+
+	public static get_epics_by_project() {
+
+	}
+
+	public static get_features_by_epic() {
+
+	}
+
+	//will definitley use
+	public static create_test_plan() {
+
+	}
+
+
+}
